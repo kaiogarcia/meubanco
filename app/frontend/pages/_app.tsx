cac89189c3232a8918046448f89094d313ce47ec
Kaio Vinicius de Oliveira Garcia <63928281+kaiogarcia@users.noreply.github.com>
2025-02-14 19:17:36 -0300

    Delete app/frontend directory

--- app/frontend/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-}
